99419dab3d94a21ff824f38eb1e70fa074ed70bf
mt <margarettom6@gmail.com>
2020-10-19 22:57:42 +0300

    add food menu

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,18 @@
       </div>
       <router-view />
     </div>
+
+    <div class="menu">
+      <h2 class="heading">Menu</h2>
+      <div class="menuNav">
+        <a href=""> Chef's Special</a>
+        <a href=""> Main Course</a>
+        <a href=""> Cake dessert</a>
+      </div>
+      <div class="menuContent">
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,34 +39,34 @@
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
   color: rgba(0, 0, 0, 0.7);
+  background: #f1f1f1;
 }
 
 .backgroundImg {
   background-image: url("./assets/edited-brooke-lark-08boynh_r_e.jpg");
   background-repeat: no-repeat;
-    background-position: center top;
+  background-position: center top;
 
   background-size: cover;
-  position: absolute;
+  margin-top: 0px;
+  /* position: absolute;
   top: 0;
   left: 0;
   right: 0;
-  height: 90vh;
+  height: 100vh; */
 }
 
 #nav {
   text-align: right;
-  margin-top: 70px;
+  padding-top: 70px;
   margin-right: 70px;
-  font-style: normal;
-  font-weight: 300;
+  font-weight: 400;
   font-size: 1.4vw;
   line-height: 28px;
   letter-spacing: 0.2em;
 }
 
 #nav a {
-  font-weight: bold;
   color: #2c3e50;
   text-decoration: none;
   margin-left: 70px;
@@ -66,26 +78,56 @@
 #nav a.router-link-exact-active {
   /* color: #D36200; */
 }
+
+
+/* Menu Section */
+
+.menu{
+  text-align: center;
+  padding: 30px;
+}
+.heading{
+  margin: 0 0 30px;
+ font-family: "Tillana", cursive;
+ font-size: 2.5vw;
+ color: #d36200;
+}
+
+.menuNav a{
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.7);
+  margin: 0 30px;
+  letter-spacing: 0.2em;
+   font-size: 1.4vw;
+
+}
+
+.menuNav a:hover{
+  color: #d36200;
+  text-decoration-line: underline;
+  text-underline-offset: 1em
+  
+}
+
+
+/* Media Queries */
 @media only screen and (max-width: 768px) {
   .backgroundImg {
     background-position: center;
   }
 }
 @media only screen and (max-width: 600px) {
-  #nav{
+  #nav {
     margin-right: 25px;
-
   }
 }
 
 @media only screen and (max-width: 500px) {
-  .backgroundImg{
+  .backgroundImg {
     height: 60vh;
   }
-  #nav{
+  #nav {
     display: none;
   }
 }
-
-
 </style>
