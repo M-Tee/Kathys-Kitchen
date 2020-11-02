131074d182046f6c64744c4c8dc505fcfd1c704e
mt <margarettom6@gmail.com>
2020-11-02 14:22:40 +0300

    Import axios and global css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+import '@/assets/css/main.css'
 
 new Vue({
   router,
