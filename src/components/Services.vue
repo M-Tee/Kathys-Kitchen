db679522e227476d10ff95adc66ca715ce78f945
mt <margarettom6@gmail.com>
2020-11-03 20:34:37 +0300

    Services section design template

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,13 +1,52 @@
 <template>
-  <div></div>
+  <div>
+    <h2 class="heading">Service</h2>
+    <div class="serviceCard">
+    <serviceCard :key="service.id" v-for=" service in services" :service="service" />
+
+    </div>
+  </div>
 </template>
 
 <script>
+import serviceCard from "./serviceCard";
 export default {
-  
-}
+  name: "Services",
+  components: {
+    serviceCard,
+  },
+  data() {
+    return {
+      services: [
+        {
+          id: 1,
+          name: "service 1",
+          description: "lorem ipsum",
+        },
+        {
+           id: 2,
+          name: "service 1",
+          description: "lorem ipsum",
+        },
+         {
+           id: 3,
+          name: "service 1",
+          description: "lorem ipsum",
+        },
+         {
+           id: 4,
+          name: "service 1",
+          description: "lorem ipsum",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style lang="stylus" scoped>
-
+<style scoped>
+.serviceCard :nth-child(2),
+.serviceCard :nth-child(4) {
+  flex-direction: row-reverse;
+}
 </style>
---
+++ src/components/serviceCard.vue
@@ -0,0 +1,40 @@
+<template>
+   <div class="container margin">
+      <img src="../assets/KathyS.jpg" alt="" />
+      <div class="service">
+        <h4 class="name"> {{service.name}} </h4>
+        <div class="description"> {{service.description}} </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "serviceCard",
+  props: ["service"]
+}
+</script>
+
+<style scoped>
+img {
+  border-radius: 20px;
+  width: 10%;
+}
+.name{
+  color: #d36200;
+    font-size: 1.3em;
+  font-weight: bold;
+}
+img,
+.service {
+  flex: 1;
+
+}
+.service {
+  white-space: normal;
+  padding: 4em;
+  margin: auto;
+}
+
+
+</style>
