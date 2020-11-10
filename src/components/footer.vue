f52a43bfe219764c7299e798b15e045e3dc96486
mt <margarettom6@gmail.com>
2020-11-10 18:31:08 +0300

    Change footer layout

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,18 +1,15 @@
 <template>
 <footer>
-  <div id="location">
+  <div class="icons">
     <img src="../assets/icons/loactionMarker.svg" alt="Location Marker">
-    <p class="">New York</p>
-  </div>
-  <div id="socials">
     <img src="../assets/icons/Insta.svg" alt="Instagram">
     <img src="../assets/icons/youtube.svg" alt="Youtube">
     <img src="../assets/icons/twitter.svg" alt="Twitter">
-  </div>
-  <div id="email">
     <img src="../assets/icons/Email.svg" alt="Email Icon">
-    <p>kathyneil@gmail.com</p>
   </div>
+  <p>
+    ©2020 m-tee
+  </p>
 </footer>
 </template>
 
@@ -24,28 +21,21 @@ export default {
 
 <style scoped>
 footer {
-  background-color: rgba(0, 0, 0, 0.7);
-  color: rgba(241, 246, 247, 0.76) !important;
+  background-color: #171717;
+  color: #f1f6f7c2 !important;
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  padding: 30px 0;
-  
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 28px 0;
 }
-#location,
-#email{
-display: inline-flex;
-align-items: baseline;
+
+footer img {
+  margin: 0 40px;
 }
+
 p {
-  margin-left: 10px;
-}
-#socials img{
-  margin: 0 20px;
-}
-#socials,
-#email {
-  align-self:flex-end
+  margin-top: 20px;
 }
 img {
   width: 25px;
